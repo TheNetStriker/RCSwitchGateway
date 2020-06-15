3a6e3352360bbd51b12478efbc51ee1159bcda45
David Masshardt <david@masshardt.ch>
2020-06-15 21:15:53 +0200

    Added default password if ota.txt does not exist.

--- PlatformIO/ota.py
+++ PlatformIO/ota.py
@@ -1,17 +1,21 @@
 Import ("env")
+import os.path
 
 #print(env.Dump())
 
-lines = open("ota.txt","r").read().splitlines()
+if os.path.isfile("ota.txt"):
+  lines = open("ota.txt","r").read().splitlines()
 
-port = lines[0]
-password = lines[1]
+  port = lines[0]
+  password = lines[1]
 
-env.Append(CPPDEFINES=[("OTA_PASSWORD", "\\\"" + password + "\\\"")])
+  env.Append(CPPDEFINES=[("OTA_PASSWORD", "\\\"" + password + "\\\"")])
 
-if env["PIOENV"] == "release":
-  env.Replace(
-      UPLOAD_PROTOCOL="espota",
-      UPLOAD_PORT=port,
-      UPLOAD_FLAGS=["--port=8266", "--auth=" + password],
-  )
+  if env["PIOENV"] == "release":
+    env.Replace(
+        UPLOAD_PROTOCOL="espota",
+        UPLOAD_PORT=port,
+        UPLOAD_FLAGS=["--port=8266", "--auth=" + password],
+    )
+else:
+  env.Append(CPPDEFINES=[("OTA_PASSWORD", "\\\"defaultPassword\\\"")])
